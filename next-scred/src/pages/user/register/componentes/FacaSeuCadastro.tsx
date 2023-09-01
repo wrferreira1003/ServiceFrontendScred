ea604076b15fb0746d0b118d6afe5ed25689b3bb
Wellington Rodrigues Ferreira <wrferreira@MacBook-Pro-de-Wellington.local>
2023-08-31 22:43:48 -0300

    31/08 as 22:16

--- next-scred/src/pages/user/register/componentes/FacaSeuCadastro.tsx
+++ next-scred/src/pages/user/register/componentes/FacaSeuCadastro.tsx
@@ -1,5 +1,8 @@
 import { useRef } from "react";
 import FooterUser from "./FooterUser";
+import userlogo from "../../../../assets/user.png";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function FacaSeuCadastro() {
   const myRef = useRef<HTMLDivElement>(null)
@@ -11,41 +14,57 @@ export default function FacaSeuCadastro() {
   }
 
   return (
-    <div className="bg-white">
-      <div className="relative isolate px-6 lg:px-12">
-        <div className="mx-auto max-w-6xl py-32 sm:py-48 lg:py-56">
-          <div className="text-left">
-            <h1 className="text-4xl font-bold tracking-tight text-blue-900 sm:text-6xl">
+    <div className="bg-white relative pl-10 max-w-7xl mx-auto mt-40">
+ 
+      <div className="text-left">
+        <h1 className="text-4xl font-bold tracking-tight text-blue-900 sm:text-6xl">
             Faça seu cadastro
-            </h1>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
-            O cadastro permite a utilização deste Portal e requer acesso com login e senha. 
-            Alguns serviços exigem o uso do Certificado Digital ICP-Brasil. 
-            No primeiro acesso o usuário deverá preencher o cadastro. 
-            Os dados serão utilizados para emissão de boleto bancário, 
-            nota fiscal, eventuais comunicações, bem como a devolução de saldos existentes. 
-            Todos os dados e transações estão absolutamente protegidos, 
-            de conformidade com nossa Política de Privacidade.
-            </p>
-            <div className="mt-10 flex items-center justify-center gap-x-6">
-              <button
-                onClick={scrollToRef}
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                COMEÇAR O CADASTRO
-              </button>
-            </div>
-            <FooterUser/>
-          </div>
-          
-          <div ref={myRef} style={{ height: '200px' }}>
+        </h1>
+            
+          <div className="flex flex-col lg:flex-row items-center justify-between gap-10 mt-10">
 
-          <h1 className="text-4xl text-center font-bold tracking-tight text-blue-900 sm:text-4xl">
-          Vamos começar!
+            <Image
+                className="h-52 w-auto"
+                src={userlogo}
+                width={520}
+                height={480}
+                alt=""
+              />
+
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+              O cadastro permite a utilização deste Portal e requer acesso com login e senha. 
+              Alguns serviços exigem o uso do Certificado Digital ICP-Brasil. 
+              No primeiro acesso o usuário deverá preencher o cadastro. 
+              Os dados serão utilizados para emissão de boleto bancário, 
+              nota fiscal, eventuais comunicações, bem como a devolução de saldos existentes. 
+              Todos os dados e transações estão absolutamente protegidos, 
+              de conformidade com nossa Política de Privacidade.
+              </p>
+              
+              
+          </div>
+
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <button
+              onClick={scrollToRef}
+              className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+            COMEÇAR O CADASTRO
+            </button>
+          </div>
+            
+          <FooterUser/>
+          
+        </div>
+          
+        <div ref={myRef} style={{ height: '200px' }}>
+
+          <h1 className=" mt-10 text-4xl text-center font-bold tracking-tight text-blue-900 sm:text-4xl">
+            Vamos começar!
           </h1>  
           
           <div className="mx-auto mt-5 grid max-w-2xl grid-cols-1 gap-x-2 gap-y-2 
-                          border-t border-gray-200 pt-10 sm:mt-5 sm:pt-10 lg:grid-cols-2">
+                          border-gray-200 pt-10 sm:mt-5 sm:pt-10 lg:grid-cols-2">
             <div className="flex items-center justify-center">
                 <button
                   onClick={scrollToRef}
@@ -69,9 +88,6 @@ export default function FacaSeuCadastro() {
               </div>
             </div>
           </div>  
-        </div>
-        
-      </div>
     </div>
   )
 }
